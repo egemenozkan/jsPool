0e9cbde1cdd903aad1e0437024167974fc5f7578
egemenozkan@gmail.com <egemenozkan@gmail.com>
2019-05-20 23:42:51 +0300

    initial

---
+++ mydate.js
@@ -0,0 +1,12 @@
+Date.prototype.addDays = function (days) {
+    return new Date(this.valueOf() + days * 864e5);
+}
+
+function convertStrToDate(str) {
+    var dArr = str.split('-');
+    return new Date(dArr[2], dArr[1], dArr[0]);
+}
+
+function convertDateToString(date) {
+    return  ('0' + date.getDate()).slice(-2) + "-" + ('0' + (date.getMonth() + 1)).slice(-2) + date.getFullYear();
+}
