27b8dea78e73b8d605666b72535d7f4e07c6b0cb
egemenozkan@gmail.com <egemenozkan@gmail.com>
2019-06-02 23:21:18 +0300

    format

--- mydate.js
+++ mydate.js
@@ -18,8 +18,8 @@ export var LocalDateTime = {
     now : function () {
         return new Date();
     },
-    of: function (year, month, day) {
-        return new Date(year, month-1,day);
+    of: function (year, month, day, hours, minutes) {
+        return new Date(year, month-1,day, hours, minutes);
     }
 }
 
@@ -37,12 +37,26 @@ Date.prototype.addDays = function(days) {
      var _array = this.valueOf();
      return LocalDate.of(_array[0], _array[1], _array[2]);
  }
+ 
+ Array.prototype.toLocalDateTime = function () {
+     var _array = this.valueOf();
+     return LocalDate.of(_array[0], _array[1], _array[2], _array[3], _array[4]);
+ }
 
- /** 2019-07-29 **/
+ /** parse: 2019-07-29 **/
  String.prototype.format = function (pattern) {
      if (pattern == "yyyy-MM-dd") {
          var _array = this.valueOf().split('-');
          return LocalDate.of(_array[0], _array[1], _array[2]);
+     }
+ }
+ /** return: 2019-07-29 **/
+ Date.prototype.format = function (pattern) {
+     if (pattern == "yyyy-MM-dd") {
+    	 var _date = this.valueOf();
+    	 _date = new Date(_date);
+    	 console.log(_date,"--");
+    	 return _date.getFullYear() + "-"  + ("0" + (_date.getMonth() + 1)).slice(-2) + "-" + ("0" + _date.getDate()).slice(-2)
      }
  }
  
